d61a3ed9e7ed0187fde620c918b6818111074857
GONiazi <komailniazi@gmail.com>
2024-09-07 14:24:12 +0500

    first comit "complete task"

---
+++ task/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HeroSectionComponent } from './pages/hero-section/hero-section.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroSectionComponent,
+  },
+];
